24f99e96b901f1325b8f6b2ab13a4defa424923c
cdennis27 <113468121+cdennis27@users.noreply.github.com>
2023-02-16 21:36:41 -0500

    first commit

---
+++ models/index.js
@@ -0,0 +1,39 @@
+
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+  
+});
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
+
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_tags'
+});
+
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tagged_products'
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
